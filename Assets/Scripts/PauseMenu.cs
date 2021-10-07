8b4a93ef227ecbe80222044582e35c3ee3142b3a
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-10-07 23:12:31 +0200

    Creating Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            } else
+            {
+                pause(); 
+            }
+        }
+    }
+
+    void resume()
+    {
+
+    }
+
+    void pause()
+    {
+
+    }
+}
