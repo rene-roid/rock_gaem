610c15c1b48d77ad7f89afcd61b6462ad28f68e2
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-10-14 22:36:12 +0200

    re coded asteroid follow and added comments

---
+++ Assets/Scripts/Asteroids/AsteroidFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidFollow : MonoBehaviour
+{
+    // Calling Variables
+    public Transform player; // Getting player pos
+    public float speed = 5f;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting distance between asteroid and player
+        Vector2 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate()
+    {
+        // Moving asteroid
+        transform.position = (Vector2)transform.position + (movement * speed * Time.deltaTime);
+    }
+}
